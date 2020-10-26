39991a7728e68acfbc1610fc423795afeadd0cba
yangduoqing <yangduoqing@hxrainbow.com>
2020-10-26 16:41:14 +0800

    添加可编译的objc调试代码模板

---
+++ 可编译objc源码/KCObjc/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  KCObjc
+//
+//  Created by Cooci on 2020/7/24.
+//
+
+#import <Foundation/Foundation.h>
+#import "LGPerson.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        
+        
+        // NSObject *objc1 = [NSObject alloc];
+        LGPerson *objc2 = [LGPerson alloc];
+        // LGPerson *objc3 = [LGPerson alloc];
+
+        NSLog(@"Hello, World!  %@",objc2);
+    }
+    return 0;
+}
